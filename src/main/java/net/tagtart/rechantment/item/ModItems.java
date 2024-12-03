420d4948c71896c7264d407ff6477478666b93e3
tagtart1 <adamtbizz@gmail.com>
2024-12-02 19:00:50 -0600

    Setup enchanted book item boilerplate

---
+++ src/main/java/net/tagtart/rechantment/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.tagtart.rechantment.item;
+
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.tagtart.rechantment.Rechantment;
+import net.tagtart.rechantment.item.custom.EnchantedBookItem;
+
+import java.awt.print.Book;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Rechantment.MOD_ID);
+
+    public static final RegistryObject<Item> ENCHANTED_BOOK = ITEMS.register("enchanted_book", () -> new EnchantedBookItem(new Item.Properties()));
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/tagtart/rechantment/item/custom/EnchantedBookItem.java
@@ -0,0 +1,9 @@
+package net.tagtart.rechantment.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class EnchantedBookItem extends Item {
+    public EnchantedBookItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
