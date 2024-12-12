6d47485c1495cee2b96f1ec9144edea584e2cd2b
tagtart1 <adamtbizz@gmail.com>
2024-12-11 20:13:21 -0600

    Implement ice aspect functionality

--- src/main/java/net/tagtart/rechantment/enchantment/IceAspectEnchantment.java
+++ src/main/java/net/tagtart/rechantment/enchantment/IceAspectEnchantment.java
@@ -1,5 +1,7 @@
 package net.tagtart.rechantment.enchantment;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -8,10 +10,19 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.FireAspectEnchantment;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class IceAspectEnchantment extends Enchantment {
     public IceAspectEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pSlots) {
         super(pRarity, pCategory, pSlots);
     }
+
+    List<Integer> IceAspectDurationTicks = Arrays.asList(
+            30, // Level 1
+            40  // Level 2
+    );
 
     @Override
     public int getMaxLevel() {
@@ -25,6 +36,13 @@ public class IceAspectEnchantment extends Enchantment {
 
     @Override
     public void doPostAttack(LivingEntity pAttacker, Entity pTarget, int pLevel) {
-        super.doPostAttack(pAttacker, pTarget, pLevel);
+        if (!(pTarget instanceof LivingEntity livingTarget)) {return;}
+        MobEffectInstance slownessEffect = new MobEffectInstance(
+                MobEffects.MOVEMENT_SLOWDOWN,
+                IceAspectDurationTicks.get(pLevel - 1),
+                2);
+        livingTarget.extinguishFire();
+        pAttacker.addEffect(slownessEffect);
+        livingTarget.addEffect(slownessEffect);
     }
 }
