03937cfe9aa932cd01dd7e312a6c8ba8802f892d
James Robertson <jimmyrobertsonv@gmail.com>
2024-12-13 19:24:29 -0600

    Boilerplate config improvements

--- src/main/java/net/tagtart/rechantment/util/AllBookProperties.java
+++ src/main/java/net/tagtart/rechantment/util/AllBookProperties.java
@@ -5,25 +5,25 @@ import net.minecraft.world.level.block.Blocks;
 // TODO: REMOVE THIS ONCE CONFIGS ARE SET UP. MOVE TO DIFFERENT CLASS.
 public class AllBookProperties {
 
-    private static BookRequirementProperties[] ALL_BOOK_PROPERTIES;
+    private static BookRarityProperties[] ALL_BOOK_PROPERTIES;
 
-    public static BookRequirementProperties[] getAllProperties() {
+    public static BookRarityProperties[] getAllProperties() {
         if (ALL_BOOK_PROPERTIES != null) {
             return ALL_BOOK_PROPERTIES;
         }
 
         // HARD CODING THESE FOR NOW. NEED TO READ FROM CONFIG LATER!
-        ALL_BOOK_PROPERTIES = new BookRequirementProperties[5];
-        ALL_BOOK_PROPERTIES[0] = new BookRequirementProperties(
+        ALL_BOOK_PROPERTIES = new BookRarityProperties[5];
+        ALL_BOOK_PROPERTIES[0] = new BookRarityProperties(
                 1.0f,
                 100,
                 0.0f,
                 4,
-                0.05f,
+                0.90f,
                 Blocks.IRON_BLOCK
         );
 
-        ALL_BOOK_PROPERTIES[1] = new BookRequirementProperties(
+        ALL_BOOK_PROPERTIES[1] = new BookRarityProperties(
                 2.0f,
                 200,
                 0.0f,
@@ -32,7 +32,7 @@ public class AllBookProperties {
                 Blocks.GOLD_BLOCK
         );
 
-        ALL_BOOK_PROPERTIES[2] = new BookRequirementProperties(
+        ALL_BOOK_PROPERTIES[2] = new BookRarityProperties(
                 3.0f,
                 400,
                 0.015f,
@@ -41,7 +41,7 @@ public class AllBookProperties {
                 Blocks.DIAMOND_BLOCK
         );
 
-        ALL_BOOK_PROPERTIES[3] = new BookRequirementProperties(
+        ALL_BOOK_PROPERTIES[3] = new BookRarityProperties(
                 4.0f,
                 500,
                 0.015f,
@@ -50,7 +50,7 @@ public class AllBookProperties {
                 Blocks.EMERALD_BLOCK
         );
 
-        ALL_BOOK_PROPERTIES[4] = new BookRequirementProperties(
+        ALL_BOOK_PROPERTIES[4] = new BookRarityProperties(
                 5.0f,
                 2000,
                 0.015f,
