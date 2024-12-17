1ade4372e2f70430ab0b3b9d3f6f657ff54151dc
tagtart1 <adamtbizz@gmail.com>
2024-12-16 23:11:38 -0600

    Set max level and canEnchant

--- src/main/java/net/tagtart/rechantment/enchantment/InquisitiveEnchantment.java
+++ src/main/java/net/tagtart/rechantment/enchantment/InquisitiveEnchantment.java
@@ -1,11 +1,23 @@
 package net.tagtart.rechantment.enchantment;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class InquisitiveEnchantment extends Enchantment {
     public InquisitiveEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pSlots) {
         super(pRarity, pCategory, pSlots);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 4;
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack pStack) {
+        return pStack.getItem() instanceof AxeItem || super.canEnchant(pStack);
     }
 }
