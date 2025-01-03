673acb77ddc6901f2800e7fb844672a2258d67ea
tagtart1 <adamtbizz@gmail.com>
2025-01-02 21:02:00 -0600

    Updated creative mode menu

--- src/main/java/net/tagtart/rechantment/item/ModCreativeModeTabs.java
+++ src/main/java/net/tagtart/rechantment/item/ModCreativeModeTabs.java
@@ -1,15 +1,24 @@
 package net.tagtart.rechantment.item;
 
+import net.minecraft.Util;
+import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.tagtart.rechantment.Rechantment;
+import net.tagtart.rechantment.util.BookRarityProperties;
+import net.tagtart.rechantment.util.EnchantmentPoolEntry;
+import net.tagtart.rechantment.util.UtilFunctions;
 
 // TODO: REMOVE THIS?
 // Just here in case it's needed for dev.
@@ -17,15 +26,43 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Rechantment.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("rechantment_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
-                    .title(Component.literal("Rechantment"))  // !! Translatable need a name defined in en_us.json!!!
+    public static final RegistryObject<CreativeModeTab> RECHANTMENT_TAB = CREATIVE_MODE_TABS.register("rechantment_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> {
+                        ItemStack icon = new ItemStack(ModItems.ENCHANTED_BOOK.get());
+                        CompoundTag rootTag = icon.getOrCreateTag();
+
+
+                        rootTag.putBoolean("IconOnly", true);
+                        return icon;
+                    })
+                    .title(Component.translatable("creative.rechantment.title"))  // !! Translatable need a name defined in en_us.json!!!
                     .displayItems((pParameters, pOutput) -> {
-
-                        // Adding Modded items to tab display in this order via accept()
-                        pOutput.accept(Blocks.ENCHANTING_TABLE);
-
                         pOutput.accept(ModItems.CHANCE_GEM.get());
+                        BookRarityProperties[] bookRarityProperties = BookRarityProperties.getAllProperties();
+
+                        for(BookRarityProperties bookRarityProperty : bookRarityProperties) {
+                            for(EnchantmentPoolEntry enchantPoolEntry : bookRarityProperty.enchantmentPool) {
+                               String enchantmentRaw = enchantPoolEntry.enchantment;
+                                Enchantment enchantment = ForgeRegistries.ENCHANTMENTS.getValue(new ResourceLocation(enchantmentRaw));
+                                if (enchantment == null) continue;
+                               int maxLevel = enchantment.getMaxLevel();
+                               for(int i = 1 ; i <= maxLevel ; i++) {
+                                   ItemStack book = new ItemStack(ModItems.ENCHANTED_BOOK.get());
+
+                                   CompoundTag rootTag = book.getOrCreateTag();
+                                   CompoundTag enchantmentTag = new CompoundTag();
+
+                                   enchantmentTag.putInt("lvl", i);
+                                   enchantmentTag.putString("id", enchantmentRaw);
+
+                                   rootTag.put("Enchantment", enchantmentTag);
+                                   rootTag.putInt("SuccessRate", 100);
+
+                                   pOutput.accept(book);
+                               }
+                            }
+                        }
                     })
                     .build());
 
