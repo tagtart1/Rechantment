a4b37fcc3743daabf6758a6f64034f91f9213037
tagtart1 <adamtbizz@gmail.com>
2025-01-08 19:50:31 -0600

    Fix annotation

--- src/main/java/net/tagtart/rechantment/sound/LoopingAmbientSound.java
+++ src/main/java/net/tagtart/rechantment/sound/LoopingAmbientSound.java
@@ -4,7 +4,11 @@ import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+
+@OnlyIn(Dist.CLIENT)
 public class LoopingAmbientSound extends AbstractTickableSoundInstance {
     private boolean shouldStop = false;
 
